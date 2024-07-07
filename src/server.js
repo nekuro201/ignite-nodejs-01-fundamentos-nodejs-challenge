f22b03102a37180ee119db81e7f062924a210eb8
nekuro201 <guilherme.kodi.abe@gmail.com>
2024-07-07 12:22:51 +0900

    add middleware to json

--- src/server.js
+++ src/server.js
@@ -1,18 +1,37 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
 const tasks = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req, res) => {
 
   const { method, url } = req
 
+  await json(req, res)
+
   if(method === 'GET' && url === '/tasks') {
     return res
-      .setHeader('Content-type', 'application/json')
       .end(JSON.stringify(tasks))
   }
 
-  return res.end('hello world')
+  if(method === 'POST' && url === '/tasks') {
+    const { title, description } = req.body
+
+    const newTask = {
+      id: '2',
+      title: title,
+      description: description,
+      completed_at: null,
+      create_at: new Date(),
+      updated_at: new Date(),
+    }
+
+    tasks.push(newTask)
+
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end('route not founded!')
 })
 
 server.listen(3333)
